c9ffd3068bcc3c431131a12e8e62482506be74c4
Anton Bredenbeck <a.bredenbeck@tudelft.nl>
2024-07-17 15:37:58 -0700

    Add mounting procedure for volume

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,18 @@
 xhost +local:root
 
+# Check if the string is empty
+if [ -z $1 ]; then
+  echo "Please provide a path to the folder rosbags"
+  exit 1
+fi
+
+# Remove the trailing slash if it exists
+input_string="$1"
+path="${input_string%/}"
+
 docker run -it --rm \
         -e "DISPLAY=$DISPLAY" \
+        -v $1/rosbags:/root/rosbags \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
         --device=/dev/dri:/dev/dri \
         plotjuggler
